0b9968f72b91a9902ce1993ef72e80fa95cefdd6
doctording <dingwenlong2014@163.com>
2020-07-23 23:52:40 +0800

    init

---
+++ code/demo/src/main/java/com/example/demo/control/ZkControl.java
@@ -0,0 +1,15 @@
+package com.example.demo.control;
+
+import com.example.demo.repository.model.TbUser;
+import com.example.demo.service.TbUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ZkControl {
+
+
+}
